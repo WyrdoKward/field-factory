7abb26b57f009bbbae24fb1993c5038bdde45848
WyrdoKward <42283252+WyrdoKward@users.noreply.github.com>
2020-09-08 16:47:56 +0200

    Create Town.vue

---
+++ vue-mpa-client/src/components/map/Town.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="town" @click="diplayTownMapWindow()" >
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Town',
+    props: {
+      loc: Object,
+    },
+    data() {
+      return {
+        outputVar: "Je suis utilisable dans tout le fichier avec this.outputVar",               
+      }
+    },
+    methods: {
+      diplayTownMapWindow() {
+        alert("In town #" + this.loc.id);
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+
+.town{
+  background-image: url(assets/map/locations/townCenter.png);
+  margin: auto;
+}
+
+</style>
