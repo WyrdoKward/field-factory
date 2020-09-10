691474adefb9f0612e8a5c37acd06550a7e7f63c
Wyrdo Kward <wyrdokward@gamal.com>
2020-09-10 23:44:07 +0200

    Choices and outcomes chaining

--- vue-mpa-client/src/components/map/LocationEvent.vue
+++ vue-mpa-client/src/components/map/LocationEvent.vue
@@ -3,19 +3,48 @@
         <h2>{{event.text}}</h2>
         <ul v-if="event.choices" class="choices">
             <li v-for="choice in event.choices" :key="choice.id">
-                <span @click="processChoice(choice.nextStepId)">{{choice.text}}</span>
+                <span @click="processChoice(choice.nextStepId)">{{choice.text}} (nextStep: {{choice.nextStepId}}</span>
             </li>
         </ul>
-        <ul v-if="event.outcomes" class="outcomes">
-            <li v-for="outcome in event.outcomes" :key="outcome.id">
+        <div v-else-if="event.outcome" class="outcome">
+            <p >{{event.outcome.text}}</p>
+            <p v-if="event.outcome.nextStepId" @click="processOutcome(event.outcome.nextStepId)">Continuer...</p>
+            <p v-else>QUETE TERMINEE</p>
+            <!--<li v-for="outcome in event.outcomes" :key="outcome.id">
                 <span @click="processOutcome()">{{outcome.text}}</span>
-            </li>
-        </ul>
+            </li>-->
+        </div>
+        <p v-else>Problème: ni outcome ni choices</p>
+         <button type="button" @click="resetQuest()" style="margin-bottom: 80px;">DEBUG : RESET QUEST</button> 
     </div>
 </template>
 
 <script>
+/*
+import { activeCountries } from '@/utils/CountryConstants'
+
+const homepageComponents = {}
+
+activeCountries.map(country => {
+  const pageName = `HomePage${country.code.toUpperCase()}`
+  homepageComponents[pageName] = () => import(`@/pages/homepage/${pageName}`)
+})
+
+*/
+
+import step0 from '@/utils/dummyLocationEventStep0.json'
 import step1 from '@/utils/dummyLocationEventStep1.json'
+import step2 from '@/utils/dummyLocationEventStep2.json'
+import step3 from '@/utils/dummyLocationEventStep3.json'
+import step4 from '@/utils/dummyLocationEventStep4.json'
+const steps = {}
+steps[0] = step0;
+steps[1] = step1;
+steps[2] = step2;
+steps[3] = step3;
+steps[4] = step4;
+
+
   export default {
     name: 'LocationEvent',
     props: {
@@ -23,20 +52,34 @@ import step1 from '@/utils/dummyLocationEventStep1.json'
     },
     data() {
       return {
-        outputVar: "Je suis utilisable dans tout le fichier avec this.outputVar",               
       }
     },
     methods: {
       processChoice(nextStepId) {
         console.log('Next Step is #'+nextStepId);
         // GET /event/{eventId}/step/{nextStepId}
-        this.event = step1;
+        //on selectionne un seul outcome random coté serveur
+        this.event = steps[nextStepId];
+        var rnd = this.getRandom(Object.keys(this.event.outcomes).length);
+        console.log('Outcome generated : '+rnd);
+        this.event.outcome = this.event.outcomes[rnd];
+
+        console.log(this.event);
 
       },
 
-      processOutcome() {
-        return this.outputVar;
+      processOutcome(nextStepId) {
+        //On get juste le step suivant
+        this.event = steps[nextStepId];
+        
+      },
+      getRandom(max){
+          return Math.floor(Math.random() * (max));
+      },
+      resetQuest(){
+          this.event = steps[0];
       }
+
     }
   }
 </script>
