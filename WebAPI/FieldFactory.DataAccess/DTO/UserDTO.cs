9072e9547a241fef9d7f08b0d743af14619256e9
Wyrdo Kward <wyrdokward@gmail.com>
2020-09-17 15:49:00 +0200

    SQLite test cnx

---
+++ WebAPI/FieldFactory.DataAccess/DTO/UserDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FieldFactory.DataAccess.DTO
+{
+    internal class UserDTO
+    {
+        public int IdUser { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
