b62505ee7adbf15c199fd619332b5ad5b7f92187
WyrdoKward <42283252+WyrdoKward@users.noreply.github.com>
2020-09-25 14:11:51 +0200

    Create SetCookie()

--- WebAPI/FieldFactory.Core/Utils/CookieHelper.cs
+++ WebAPI/FieldFactory.Core/Utils/CookieHelper.cs
@@ -4,10 +4,23 @@ namespace FieldFactory.Core.Utils
 {
     public static class CookieHelper
     {
+        private const int AUTH_COKKIE_EXPIRE_TIME_H = 12;
+        
         public static string GetFromCookie(HttpContext httpContext, string cookieName)
         {
             string cookie = httpContext.Request.Cookies[cookieName];
             return cookie;
         }
+        
+        
+        public static void SetCookie(HttpContext httpContext, string cookieName, string cookieValue)
+        {
+           CookieOptions options = new CookieOptions();  
+           options.Expires = DateTime.Now.AddHours(AUTH_COKKIE_EXPIRE_TIME_H);  
+           options.HttpOnly = true;
+
+           Response.Cookies.Append(cookieName, cookieValue, options); 
+        }
     }
+    
 }
