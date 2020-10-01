54d6a388dd1f7d50e002438e0686898a75555437
Wyrdo Kward <wyrdokward@gmail.com>
2020-10-01 17:54:23 +0200

    Add Test Project

---
+++ WebAPI/FieldFactory.Tests/Core/Entities/Test_EventRelatedEntities.cs
@@ -0,0 +1,32 @@
+using FieldFactory.Core.Entities.Map.Event;
+using NUnit.Framework;
+
+namespace FieldFactory.Tests.Core.Entities
+{
+    public class Tests
+    {
+        Choice c = new Choice();
+
+        [SetUp]
+        public void Setup()
+        {
+            c.Outcomes = new Outcome[3]
+            {
+                new Outcome(){NextStepId = 1, Weight = 1 },
+                new Outcome(){NextStepId = 2, Weight = 1 },
+                new Outcome(){NextStepId = 3, Weight = 2 },
+            };
+        }
+
+        [Test]
+        [TestCase(0, 1)]
+        [TestCase(1, 2)]
+        [TestCase(2, 3)]
+        [TestCase(3, 3)]
+        public void Returns_valid_nextStepId(int rndIndex, int expectedNextstepId)
+        {
+            int actual = c.ChooseRandomNextStep(rndIndex);
+            Assert.AreEqual(expectedNextstepId, actual);
+        }
+    }
+}
