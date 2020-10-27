b5c5b621239eab655a5682b28f7f65b661de6dba
Wyrdo Kward <wyrdokward@gmail.com>
2020-10-27 23:48:37 +0100

    Add Db Tests

---
+++ WebAPI/FieldFactory.Tests/DataAccess/Mock/TestProvider.cs
@@ -0,0 +1,15 @@
+﻿using FieldFactory.DataAccess.SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FieldFactory.Tests.DataAccess.Mock
+{
+    public class TestProvider : SQLiteBaseProvider
+    {
+        public Dictionary<int, Dictionary<string, string>> ReadColumnsMock(string query)
+        {
+            return ReadColumns(query, 3);
+        }
+    }
+}
