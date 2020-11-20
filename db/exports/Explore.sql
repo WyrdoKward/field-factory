96164c374ec6bcfeee5fb5c0118ea4e1aec349db
Wyrdo Kward <wyrdokward@gmail.com>
2020-11-20 21:40:11 +0100

    Maj exports DB

---
+++ db/exports/Explore.sql
@@ -0,0 +1,3 @@
+DROP TABLE IF EXISTS "Explore";
+CREATE TABLE "Explore" ("idPlayer" text,"idFollower" text,"idLocation" text,"idEvent" text,"idStep" integer,"dateNextStep" datetime, "stepsHistory" text, "idChoice" text);
+
---
+++ db/exports/Provide.sql
@@ -0,0 +1,3 @@
+DROP TABLE IF EXISTS "Provide";
+CREATE TABLE "Provide" ("idPlayer" text,"idFurniture" text,"newLevel" integer,"dateEnd" datetime);
+
