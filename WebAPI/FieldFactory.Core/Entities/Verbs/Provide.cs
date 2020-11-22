8d117e9ab3070f02bda82e96e486c1a929abfa9a
Wyrdo Kward <wyrdokward@gmail.com>
2020-11-22 14:33:16 +0100

    Provide

---
+++ WebAPI/FieldFactory.Core/Entities/Verbs/Provide.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace FieldFactory.Core.Entities.Verbs
+{
+    public class Provide
+    {
+        #region Members
+        public string IdPlayer { get; set; }
+        public string IdFurniture { get; set; }
+        public int NewLevel { get; set; }
+        /// <summary>
+        /// Returns friendly string date format for serialization
+        /// </summary>
+        public string DateEnd
+        {
+            get
+            {
+                return m_dateEnd.ToString("yyyy/MM/dd HH:mm:ss");
+            }
+        }
+
+        #endregion
+
+        #region Getters&Setters
+
+        private DateTime m_dateEnd;
+        //Getter et Setter with DateTime
+        public void SetDateEnd(DateTime d)
+        {
+            m_dateEnd = d;
+        }
+
+        public DateTime GetDateEnd()
+        {
+            return m_dateEnd;
+        }
+
+
+        #endregion
+    }
+}
---
+++ WebAPI/FieldFactory.DataAccess/DTO/ProvideDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FieldFactory.DataAccess.DTO
+{
+    public class ProvideDTO
+    {
+        public string IdPlayer { get; set; }
+        public string IdFurniture { get; set; }
+        public int NewLevel { get; set; }
+        public DateTime DateEnd { get; set; }
+
+        public ProvideDTO(string idPlayer, string idFurniture, int newLevel, DateTime dateEnd)
+        {
+            IdPlayer = idPlayer;
+            IdFurniture = idFurniture;
+            NewLevel = newLevel;
+            DateEnd = dateEnd;
+        }
+    }
+}
