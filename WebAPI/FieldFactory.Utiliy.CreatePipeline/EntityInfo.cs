a46592e957730d721b307ba9cc783e2c6172b4f1
Wyrdo Kward <wyrdokward@gmail.com>
2020-11-30 20:58:45 +0100

    Ajustments

--- WebAPI/FieldFactory.Utiliy.CreatePipeline/EntityInfo.cs
+++ WebAPI/FieldFactory.Utiliy.CreatePipeline/EntityInfo.cs
@@ -6,14 +6,18 @@ namespace test_dotnet
     {
         public string Name;
         public string NamePattern;
-        public string Project;
+        public string ProjectDir;
+        public string ProjectFile;
         public string FolderPath;
+
+        private string _rootSlnFolder = "C:\\Users\\pierr\\Documents\\Code\\Repos\\field-factory\\WebAPI";
 
         public EntityInfo(string name, string namePattern, string project, string folderPath){
             Name = name;
             NamePattern = namePattern;
-            Project = $"{project}/{project}.csproj";
-            FolderPath = $"{project}/{folderPath}/";;
+            ProjectDir = $"{_rootSlnFolder}\\{project}\\";
+            ProjectFile = $"{ProjectDir}\\{project}.csproj";
+            FolderPath = $"{ProjectDir}\\{folderPath}\\";
         }
 
         public string GetFilePath(){
