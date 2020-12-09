442f8b628fa4743a9cf3fc0f5e34e60e7109b385
Wyrdo Kward <wyrdokward@gmail.com>
2020-12-09 15:34:37 +0100

    Ajout FurnitureInteractor

---
+++ WebAPI/FieldFactory.Core/Headquarters/FurnitureInteractor.cs
@@ -0,0 +1,30 @@
+﻿using FieldFactory.Core.Entities.Headquarters;
+using FieldFactory.DataAccess.Providers;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FieldFactory.Core.Headquarters
+{
+    public class FurnitureInteractor
+    {
+        FurnitureProvider furnitureProvider = new FurnitureProvider();
+        //PlayerHqProvider playerHqProvider = new PlayerHqFurnitureProvider();
+
+        public List<Furniture> GetPlayerFurnitures(string playerId)
+        {
+            //Récupérer la ligne qui correspond aux constructions faites par la player dans son HQ
+            //var playerHqDto = playerHqProvider.Get(playerId);
+
+            //On récupère toutes les fournitures construites dans le HQ
+            /*var furnitureDto = furnitureProvider.Get(playerId);
+            var furniture = JsonConvert.DeserializeObject<Furniture>(furnitureDto.Json);*/
+
+            //Pour chaque furniture on récupère la description qui correspond à son level
+            List<Furniture> furnitures = null;
+
+            return furnitures;
+        }
+    }
+}
